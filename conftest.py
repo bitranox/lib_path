3daf71b5f7d90a4000acec79abc14fad2137673e
Robert Nowotny <bitranox@users.noreply.github.com>
2019-05-06 01:09:12 +0200

    initial

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
 # ignore for pytest collection
 import sys
-collect_ignore = []
+collect_ignore = ["build_docs.py"]
+"""
 if sys.version_info < (3, 5):
     collect_ignore.append("build_docs.py")
+"""
