d4af36d12b5dc94c94d31f22630c923621555e64
bitranox <rnowotny1966@gmail.com>
2019-08-27 20:26:17 +0200

    refractor

--- __init__.py
+++ __init__.py
@@ -1,1 +1,5 @@
-from .lib_path import *
+try:
+    from .lib_path import *
+# this we need for pip install --install-option test
+except ImportError:
+    import lib_path
--- lib_path/__init__.py
+++ lib_path/__init__.py
@@ -1,5 +1,13 @@
-__title__ = 'lib_path'
-__version__ = '1.0.1'
-__name__ = 'lib_path'
-
+import pathlib
 from .lib_path import *
+
+
+def get_version() -> str:
+    with open(pathlib.Path(__file__).parent / 'version.txt', mode='r') as version_file:
+        version = version_file.readline()
+    return version
+
+
+__title__ = 'lib_path'
+__version__ = get_version()
+__name__ = 'lib_path'
