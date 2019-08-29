2e310e113d0677abb62be330f2cd083858e54a1e
bitranox <rnowotny1966@gmail.com>
2019-08-30 00:24:21 +0200

    refractor

--- lib_path/__init__.py
+++ lib_path/__init__.py
@@ -3,7 +3,7 @@ from .lib_path import *
 
 
 def get_version() -> str:
-    with open(pathlib.Path(__file__).parent / 'version.txt', mode='r') as version_file:
+    with open(str(pathlib.Path(__file__).parent / 'version.txt'), mode='r') as version_file:
         version = version_file.readline()
     return version
 
